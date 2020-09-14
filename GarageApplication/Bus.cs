7a0d65a0977efbd406c48916d1b127c11aa2fbb8
Jansan <jansan-26@hotmail.com>
2020-09-14 13:26:55 +0200

    Bus class and property added

---
+++ GarageApplication/Bus.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageApplication
+{
+    class Bus : Vehicle
+    {
+        private string fueltype;
+
+        public string Fueltype
+        {
+            get { return fueltype; }
+            set { fueltype = value; }
+        }
+        private int numberOfSeats;
+
+        public int NumberOfSeats
+        {
+            get { return numberOfSeats; }
+            set { numberOfSeats = value; }
+        }
+        private double lenght;
+
+        public double Lenght
+        {
+            get { return lenght; }
+            set { lenght = value; }
+        }
+
+        public Bus(string registrationNumber, string color, int numberOfWheels, string fueltype, int numberOfSeats, double lenght) : base(registrationNumber, color, numberOfWheels)
+        {
+            Fueltype = fueltype;
+            NumberOfSeats = NumberOfSeats;
+            Lenght = lenght;
+        }
+    }
+}
