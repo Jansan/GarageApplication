6dbdd1fdd025be2827aa7d414449dc3ef1d1155e
Jansan <jansan-26@hotmail.com>
2020-09-14 13:35:50 +0200

    Boat class and property added

---
+++ GarageApplication/Boat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageApplication
+{
+    class Boat : Vehicle
+    {
+        private int numberOfEngines;
+
+        public int NumberOfEngines
+        {
+            get { return numberOfEngines; }
+            set { numberOfEngines = value; }
+        }
+        private int cylinderVolume;
+
+        public int CylinderVolume
+        {
+            get { return cylinderVolume; }
+            set { cylinderVolume = value; }
+        }
+        private string fueltype;
+
+        public string Fueltype
+        {
+            get { return fueltype; }
+            set { fueltype = value; }
+        }
+
+        public Boat(string registrationNumber, string color, int numberOfWheels, int numberOfEngines, int cylinderVolume, string fueltype) : base(registrationNumber, color, numberOfWheels)
+        {
+            NumberOfEngines = numberOfEngines;
+            CylinderVolume = cylinderVolume;
+            Fueltype = fueltype;
+
+        }
+    }
+}
