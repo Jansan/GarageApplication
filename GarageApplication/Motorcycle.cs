9f599fc82297cfda7714c7c0b8d8a2c0ed02a571
Jansan <jansan-26@hotmail.com>
2020-09-14 13:10:55 +0200

    Motorcycle class and property added

---
+++ GarageApplication/Motorcycle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageApplication
+{
+    class Motorcycle : Vehicle
+    {
+        private int cylinderVolume;
+
+        public int CylinderVolume
+        {
+            get { return cylinderVolume; }
+            set { cylinderVolume = value; }
+        }
+        private string fueltype;
+
+        public string Fueltype
+        {
+            get { return fueltype; }
+            set { fueltype = value; }
+        }
+        private int numberOfSeats;
+
+        public int NumberOfSeats
+        {
+            get { return numberOfSeats; }
+            set { numberOfSeats = value; }
+        }
+
+        public Motorcycle(string registrationNumber, string color, int numberOfWheels, int cylinderVolume, string fueltype, int numberOfSeats) : base(registrationNumber, color, numberOfWheels)
+        {
+            CylinderVolume = cylinderVolume;
+            Fueltype = fueltype;
+            NumberOfSeats = numberOfSeats;
+        }
+    }
+}
